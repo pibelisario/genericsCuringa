fd5717d4adbd73485e472e49dbd0544a5a4664ad
Paulo Inácio <pibelisario@gmail.com>
2022-09-09 10:50:57 -0300

    Inicital commit

---
+++ src/app/Program.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> myInts = Arrays.asList(5,2,10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Paulo", "Clau", "João");
+		printList(myStrs);
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+
+}
